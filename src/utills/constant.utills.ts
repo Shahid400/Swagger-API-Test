a83ceef3e3284df13a9ac27f4de4564883bc54cb
Shahid400 <shahid.mehmood@ceative.co.uk>
2021-10-12 13:10:31 +0500

    First commit

---
+++ src/utills/constant.utills.ts
@@ -0,0 +1,5 @@
+export const MongoCluster = "cluster0.mvwkz.mongodb.net";
+export const MongoDbName = "ResturauntDB";
+export const MongoUserName = "Shahid";
+export const MongoPass = "shahid123";
+
