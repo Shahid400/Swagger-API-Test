9fb4ffbdf5b37a766c686ff2c47d02b69ad3ae5d
Shahid400 <92322816+Shahid400@users.noreply.github.com>
2021-12-25 03:31:51 +0500

    Update index.routes.ts

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { AdminRoutesApi } from './admin.routes';
 import { ResturauntRoutesApi } from './resturaunt.routes';
 import { UserRoutesApi } from './user.routes';
+import { WeatherRoutesApi } from './weather.routes';
 export class MainRoutes{
     router:express.Router;
     constructor(){
@@ -12,7 +13,7 @@ export class MainRoutes{
         this.router.use('/admin', AdminRoutesApi)
         this.router.use('/user', UserRoutesApi)
         this.router.use('/resturaunt', ResturauntRoutesApi)
-        
+        this.router.use('/weather', WeatherRoutesApi)
     }
 }
 export const MainApi = new MainRoutes().router;
